8a4437d1ce8bbb51b5ff164b2d677b0451580891
itsthankk <tkingsway3@gmail.com>
2023-11-28 15:50:06 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+// require('@openzeppelin/hardhat-upgrades');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    }
+  }
 };
